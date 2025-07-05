522644b617cdaed997e55d0865d62b20767c202a
Maha2525 <madisettymaha1208@gmail.com>
2025-07-05 15:54:43 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -4,13 +4,13 @@ sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-17-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
-sudo wget https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.78.2-04.tar.g
-sudo tar -xvf nexus-unix-x86-64-3.78.2-04.tar.gz
+sudo wget https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.79.0-09.tar.g
+sudo tar -xvf nexus-unix-x86-64-3.79.0-09.tar.gz
 sudo mv nexus-unix* nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
 sudo chown -R nexus:nexus /app/sonatype-work
-sudo echo "run_as_user="nexus"" > /app/nexus-3.78.2-04/bin/nexus.rc
+sudo echo "run_as_user="nexus"" > /app/nexus-3.79.0-09/bin/nexus.rc
 sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
 [Unit] 
 Description=nexus service
@@ -21,8 +21,8 @@ Type=forking
 LimitNOFILE=65536
 User=nexus
 Group=nexus
-ExecStart=/app/nexus-3.78.2-04/bin/nexus start
-ExecStop=/app/nexus-3.78.2-04/bin/nexus stop
+ExecStart=/app/nexus-3.79.0-09/bin/nexus start
+ExecStop=/app/nexus-3.79.0-09/bin/nexus stop
 User=nexus
 Restart=on-abort
 
@@ -32,4 +32,3 @@ EOL
 sudo chkconfig nexus on
 sudo systemctl start nexus
 sudo systemctl status nexus
-
